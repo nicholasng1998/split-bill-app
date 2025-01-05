50563dc3aa7a7f37466210a7ee3bd03752a22772
NicholasNg <52874951+nicholasng1998@users.noreply.github.com>
2025-01-05 13:28:40 +0800

    update group UI

--- flutter_application/lib/pages/widgets/groups.dart
+++ flutter_application/lib/pages/widgets/groups.dart
@@ -28,18 +28,19 @@ class GroupsScreenState extends State<GroupsScreen> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        padding: const EdgeInsets.only(top: 23.0),
-        child: Center(
-            child: Column(children: <Widget>[
-          Text(
-            "Groups",
-            style: const TextStyle(
-                fontFamily: "WorkSansSemiBold",
-                fontSize: 25.0,
-                color: Colors.black),
-          ),
-          Flexible(
-            child: Container(
+      padding: const EdgeInsets.only(top: 23.0),
+      child: Center(
+        child: Column(
+          children: <Widget>[
+            Text(
+              "Groups",
+              style: const TextStyle(
+                  fontFamily: "WorkSansSemiBold",
+                  fontSize: 25.0,
+                  color: Colors.black),
+            ),
+            Flexible(
+              child: Container(
                 margin: const EdgeInsets.only(top: 20.0),
                 width: 350.0,
                 height: 450.0,
@@ -50,105 +51,134 @@ class GroupsScreenState extends State<GroupsScreen> {
                     borderRadius: BorderRadius.circular(8.0),
                   ),
                   child: SingleChildScrollView(
-                    child: Column(children: <Widget>[
-                      ...groupList.map((group) {
-                        return Padding(
-                            padding: EdgeInsets.symmetric(
-                                vertical: 10.0, horizontal: 15.0),
-                            child: GestureDetector(
-                              onTap: () {
-                                Navigator.pushNamed(
-                                  context,
-                                  '/groupDetailsScreen',
-                                  arguments:
-                                      group, // Passing the group object as an argument
-                                );
-                              },
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  color:
-                                      Colors.white, // Button background color
-                                  borderRadius: BorderRadius.circular(
-                                      8.0), // Rounded corners
-                                  border: Border.all(
-                                    color: Colors.blue, // Border color
-                                    width: 2.0, // Border width
-                                  ),
-                                  boxShadow: [
-                                    BoxShadow(
-                                      color: Colors.grey.withOpacity(0.3),
-                                      spreadRadius: 1,
-                                      blurRadius: 5,
-                                      offset: Offset(0, 3), // Shadow position
+                    child: Column(
+                      children: <Widget>[
+                        ...groupList.map(
+                          (group) {
+                            return Padding(
+                              padding: EdgeInsets.symmetric(
+                                  vertical: 10.0, horizontal: 15.0),
+                              child: GestureDetector(
+                                onTap: () {
+                                  Navigator.pushNamed(
+                                    context,
+                                    '/groupDetailsScreen',
+                                    arguments:
+                                        group, // Passing the group object as an argument
+                                  );
+                                },
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    color:
+                                        Colors.white, // Button background color
+                                    borderRadius: BorderRadius.circular(
+                                        8.0), // Rounded corners
+                                    border: Border.all(
+                                      color: Colors.blue, // Border color
+                                      width: 2.0, // Border width
                                     ),
-                                  ],
-                                ),
-                                padding: EdgeInsets.symmetric(
-                                    vertical: 12.0, horizontal: 20.0),
-                                child: Align(
-                                  alignment: Alignment.centerLeft,
-                                  child: Text(
-                                    "${group.groupName} - RM${group.totalExpenses}",
-                                    style: const TextStyle(
-                                        fontFamily: "WorkSansSemiBold",
-                                        fontSize: 16.0,
-                                        color: Colors.black),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Colors.grey.withOpacity(0.3),
+                                        spreadRadius: 1,
+                                        blurRadius: 5,
+                                        offset: Offset(0, 3), // Shadow position
+                                      ),
+                                    ],
+                                  ),
+                                  padding: EdgeInsets.symmetric(
+                                      vertical: 12.0, horizontal: 20.0),
+                                  child: Align(
+                                    alignment: Alignment.centerLeft,
+                                    child: itemListText(group),
                                   ),
                                 ),
                               ),
-                            ));
-                      })
-                    ]),
+                            );
+                          },
+                        ),
+                      ],
+                    ),
                   ),
-                )),
-          ),
-          Container(
-            margin: const EdgeInsets.only(top: 40.0),
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(5.0)),
-              boxShadow: <BoxShadow>[
-                BoxShadow(
-                  color: CustomTheme.loginGradientStart,
-                  offset: Offset(1.0, 6.0),
-                  blurRadius: 20.0,
-                ),
-                BoxShadow(
-                  color: CustomTheme.loginGradientEnd,
-                  offset: Offset(1.0, 6.0),
-                  blurRadius: 20.0,
-                ),
-              ],
-              gradient: LinearGradient(
-                  colors: <Color>[
-                    CustomTheme.loginGradientEnd,
-                    CustomTheme.loginGradientStart
-                  ],
-                  begin: FractionalOffset(0.2, 0.2),
-                  end: FractionalOffset(1.0, 1.0),
-                  stops: <double>[0.0, 1.0],
-                  tileMode: TileMode.clamp),
-            ),
-            child: MaterialButton(
-              highlightColor: Colors.transparent,
-              splashColor: CustomTheme.loginGradientEnd,
-              child: const Padding(
-                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 42.0),
-                child: Text(
-                  'Create Group',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 25.0,
-                      fontFamily: 'WorkSansBold'),
                 ),
               ),
-              onPressed: () => {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => AddGroupScreen()),
-                )
-              },
             ),
-          )
-        ])));
+            Container(
+              margin: const EdgeInsets.only(top: 40.0),
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                boxShadow: <BoxShadow>[
+                  BoxShadow(
+                    color: CustomTheme.loginGradientStart,
+                    offset: Offset(1.0, 6.0),
+                    blurRadius: 20.0,
+                  ),
+                  BoxShadow(
+                    color: CustomTheme.loginGradientEnd,
+                    offset: Offset(1.0, 6.0),
+                    blurRadius: 20.0,
+                  ),
+                ],
+                gradient: LinearGradient(
+                    colors: <Color>[
+                      CustomTheme.loginGradientEnd,
+                      CustomTheme.loginGradientStart
+                    ],
+                    begin: FractionalOffset(0.2, 0.2),
+                    end: FractionalOffset(1.0, 1.0),
+                    stops: <double>[0.0, 1.0],
+                    tileMode: TileMode.clamp),
+              ),
+              child: MaterialButton(
+                highlightColor: Colors.transparent,
+                splashColor: CustomTheme.loginGradientEnd,
+                child: const Padding(
+                  padding:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 42.0),
+                  child: Text(
+                    'Create Group',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25.0,
+                        fontFamily: 'WorkSansBold'),
+                  ),
+                ),
+                onPressed: () => {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => AddGroupScreen()),
+                  )
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget itemListText(ExpensesGroupModel expensesGroupModel) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Group Name: ${expensesGroupModel.groupName.toUpperCase()}',
+              style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+            ),
+            Container(
+              width: 250,
+              child: Text(
+                'Total Expenses: RM${expensesGroupModel.totalExpenses.toStringAsFixed(2)}',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.normal),
+                softWrap: true,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
